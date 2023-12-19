e177ae9998c9a509cadf3686297b1e9cdaccab49
kalyanaalk <kanza.putri@yahoo.com>
2023-12-19 23:16:17 +0700

    add verify button

--- resources/views/vieu.blade.php
+++ resources/views/vieu.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,7 +8,12 @@
     <link rel="stylesheet" href="{{ asset('css/view.css') }}"> <!-- Gantilah "form.css" dengan nama file CSS Anda -->
     <!-- Tautan font Google Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@500&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@500&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@500&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </head>
+
 <body>
     <header>
         <h1>Dashboard</h1>
@@ -18,7 +24,7 @@
         </ul>
     </nav>
     <main>
-        @if ($latestInfo) 
+        @if ($latestInfo)
         <section class="personal-data">
             <h2>Data Pribadi Anda</h2>
             <div class="data-row">
@@ -147,9 +153,9 @@
                 <br>
                 <br>
                 @if ($latestInfo->kkDocument)
-                    <embed src="{{ asset(str_replace('public', 'storage', $latestInfo->kkDocument)) }}" type="application/pdf" width="400" height="300">
+                <embed src="{{ asset(str_replace('public', 'storage', $latestInfo->kkDocument)) }}" type="application/pdf" width="400" height="300">
                 @else
-                    <p>Tidak ada file yang diunggah</p>
+                <p>Tidak ada file yang diunggah</p>
                 @endif
             </div>
         </section>
@@ -161,19 +167,54 @@
                 <br>
                 <br>
                 @if ($latestInfo->photo1)
-                    <img src="{{ asset(str_replace('public', 'storage', $latestInfo->photo1)) }}" alt="Your PDF" width="400" height="300">
+                <img src="{{ asset(str_replace('public', 'storage', $latestInfo->photo1)) }}" alt="Your PDF" width="400" height="300">
                 @else
-                    <p>Tidak ada foto yang diunggah</p>
+                <p>Tidak ada foto yang diunggah</p>
                 @endif
             </div>
         </section>
+
+        <section class="photo-upload">
+            <td class="text-center py-3">
+                <form>
+                    @csrf
+                    <div class="form-group">
+                        <!-- <input type="hidden" name="info_id" value="{{ $info->id }}"> -->
+                        <!-- <input type="hidden" name="kk?????" value="{{ $latestInfo->kkDocument }}"> -->
+                        <button type="button" class="btn lihat px-2 py-1 dist" data-bs-toggle="modal" data-bs-target="#exampleModal">Verify</button>
+                    </div>
+                </form>
+            </td>
+        </section>
+
         @else
-            <p> No data showed </p>
+        <p> No data showed </p>
         @endif
+
+        <!-- Modal -->
+        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <!-- @if (datanya diverify) -->
+                        <!-- <p>Hash: {{ }}</p>
+                        <p>Block: {{ }}</p>
+                        <p>Issuer: {{ }}</p>
+                        <p>Signature data: {{ }}</p>
+                        @else
+                        <p>Verifikasi gagal.</p>
+                        @endif -->
+                    </div>
+                </div>
+            </div>
     </main>
 
     <footer>
         &copy; 2023
     </footer>
 </body>
+
 </html>
